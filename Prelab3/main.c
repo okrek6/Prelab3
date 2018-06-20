3d1649893b9fce99993e211df59ba6b5c78a1fa5
Brendan Krekeler <krekeler@mwc-136238.dhcp.missouri.edu>
2018-06-20 10:24:03 -0500

    First commit

---
+++ Prelab3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Prelab3
+//
+//  Created by Brendan Krekeler on 6/20/18.
+//  Copyright © 2018 Brendan Krekeler. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
